581c8bdeece7bda4706a4d6aa67a5be3821c9a9a
soulripper13 <55677941+soulripper13@users.noreply.github.com>
2025-10-13 17:19:47 +0800

    Create const.py

---
+++ custom_components/waviot_updater/const.py
@@ -0,0 +1,7 @@
+DOMAIN = "waviot_updater"
+
+CONF_API_KEY = "api_key"
+CONF_MODEM_ID = "modem_id"
+
+API_BASE = "https://lk.curog.ru/api.data"
+# Note: may need adjustment if your working URL is slightly different
