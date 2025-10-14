ecfc362130d68dafb1cc72c199b2e2a2c117a9f9
soulripper13 <55677941+soulripper13@users.noreply.github.com>
2025-10-15 00:15:24 +0800

    Update const.py

--- custom_components/waviot_updater/const.py
+++ custom_components/waviot_updater/const.py
@@ -2,4 +2,4 @@ DOMAIN = "waviot_updater"
 CONF_API_KEY = "api_key"
 CONF_MODEM_ID = "modem_id"
 UPDATE_INTERVAL = 900  # fetch every 15 minutes
-BASE_URL = "https://lk.curog.ru/api.data/"
+BASE_URL = "https://lk.curog.ru/api."
